81b7426e76760c186a6c19f2f3da68be4f94b49c
Victor Passos <131535916+vSteps@users.noreply.github.com>
2024-11-06 11:17:36 +0000

    :white_check_mark: test: Arquivos de teste agora funcionando 100%

--- sum/sum.test.js
+++ sum/sum.test.js
@@ -4,3 +4,10 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
+test('adds 17 + 52 to equal 69', () => {
+  expect(sum(17, 52)).toBe(69);
+});
+
+test('adds 1 + -2 to equal -1', () => {
+  expect(sum(1, -2)).toBe(-1);
+});
