bd25b10d0136218bd2b20b31547da62c64d4c6aa
Victor Passos <131535916+vSteps@users.noreply.github.com>
2024-11-06 11:04:47 +0000

    :sparkles: feat: Adicionado a função de fatorial e os devidos testes

---
+++ factorial.test.js
@@ -0,0 +1,19 @@
+const factorial = require('./factorial');
+
+test('test the factorial of the number 4', () => {
+    expect(factorial(4)).toBe(24);
+  });
+
+  test('test the factorial of the number 10', () => {
+    expect(factorial(10)).toBe(3628800);
+  });
+  
+  test('test the factorial of the number 1', () => {
+    expect(factorial(1)).toBe(1);
+  });
+
+  test('test the factorial of the number 0', () => {
+    expect(factorial(0)).toBe(1);
+  });
+  
+  
